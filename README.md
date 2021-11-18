308f480a5ca9928942ca2c9d5bbc5e80837637f6
Bastien <bastien.quin@hotmail.com>
2021-11-19 00:18:00 +0100

    [*] Updating README + Title

--- README.md
+++ README.md
@@ -2,7 +2,10 @@
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
-It allows you to see all the donations for the 2021 Z-Event
+It allows you to see all infos for the 2021 Z-Event
+
+
+> Here is the website : [Z-Event Results](https://z-event-results.netlify.app/)
 
 ## Available Scripts
 
--- src/Title.js
+++ src/Title.js
@@ -12,7 +12,7 @@ class Title extends React.Component {
                 <button id="toggleDarkMode" className={"px-5 py-2 border border-gray-400 bg-transparent text-center rounded-lg text-gray-400 float-right mr-12 mt-2 transition dark:hover:border-gray-100 dark:hover:text-gray-100 hover:border-gray-900 hover:text-gray-900"}>
                     <i className="fas fa-sun" id="iconDarkMode" />
                 </button>
-                <h1 className={"text-2xl font-bold dark:text-gray-100"}>Z-Event 2021 Infos</h1>
+                <h1 className={"text-2xl font-bold dark:text-gray-100"}>Z-Event 2021 Results</h1>
                 <p className={"text-gray-400"}>React app to view all details for each Z-Event 2021 streamer</p>
             </div>
         );
